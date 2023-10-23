54c83579466b66ef64d6bab6d1579e3cb47de3a7
Himilsinh-Sindha <himilsinh07@gmail.com>
2023-10-23 18:57:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use("/api/material", require("./routes/material"));
 app.use("/api/notice", require("./routes/notice"));
 app.use("/api/subject", require("./routes/subject"));
 app.use("/api/marks", require("./routes/marks"));
-app.use("/api/branch", require("./routes/branch"));
 
 app.listen(port, () => {
   console.log(`Server Listening On http://localhost:${port}`);
